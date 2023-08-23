0974242ba94632bb5d7f3c848f0897ff9393164f
ravi barla <ravibarla1@gmail.com>
2023-08-23 17:27:23 +0530

    adding html external file

--- server.js
+++ server.js
@@ -3,13 +3,26 @@
 //1. import http/mode library
 const http = require("http");
 
-//2. create server
-const server = http.createServer((req, res) => {
-  //here comes the request
-  res.end("welcome to node");
-});
+//2. creating a port
+const port = 8000;
 
-//3. specify a port to listen client request
-server.listen(3100, () => console.log("server is listening in port 3100"));
+//3. create file system
+const fs = require("fs");
 
-// console.log("server is listening in port 3100");
+//4. create request listener function
+function requestListener(req, res) {
+  console.log(req.url);
+  res.writeHead(200, { "content-type": "text/html" });
+  fs.readFile("./index.html", (err, data) => {
+    if (err) {
+      console.log('error :',err);
+      return res.end('<h1>Error  </h1>')
+    }
+return res.end(data)
+  });
+}
+//5. create server
+const server = http.createServer(requestListener);
+
+//6. specify a port to listen client request
+server.listen(port, () => console.log("server is listening in port 8000"));
